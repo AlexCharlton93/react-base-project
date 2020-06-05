21a1db589749308d2a36c239b5efaf734ad9c66e
alexcharlton93 <alex-charlton@live.co.uk>
2020-06-05 20:41:14 +0100

    Added styled components npm package with example usage on Home.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import Header from './common/header/Header';
-import Routes from './common/routes/Routes';
+import Header from "./common/header/Header";
+import Routes from "./common/routes/Routes";
 
 export default function App() {
   return (
-    <>
-      <Router>
-        <Header />
-          <Routes />
-      </Router>
-    </>
+    <Router>
+      <Header />
+      <Routes />
+    </Router>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from 'styled-components';
+
+const HomeWrapper = styled.section`
+  p {
+    font-size: 20px;
+  }
+`;
 
 export default function Home() {
   return (
-    <p>Home</p>
+    <HomeWrapper>
+      <p>Home</p>
+    </HomeWrapper>
   );
 }
