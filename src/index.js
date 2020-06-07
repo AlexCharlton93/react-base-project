76bb89bd0d72f5167b9e584d92f3b3077d92a6dd
alexcharlton93 <alex-charlton@live.co.uk>
2020-06-07 16:34:39 +0100

    Added redux-persist to app to persist state on page reloads

--- src/index.js
+++ src/index.js
@@ -1,29 +1,44 @@
-import React from 'react';
-import { render } from 'react-dom'
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
-import { createLogger } from 'redux-logger'
-import thunk from 'redux-thunk'
-import reducer from './reducers'
-import './scss/index.scss';
-import App from './App';
+import React from "react";
+import { render } from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import reducer from "./reducers";
+import { PersistGate } from "redux-persist/integration/react";
+import "./scss/index.scss";
+import App from "./App";
 import { formsQuery } from "./actions/forms";
 
-const middleware = [ thunk ];
-if (process.env.NODE_ENV !== 'production') {
-  middleware.push(createLogger());
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const middleware = [thunk];
+if (process.env.NODE_ENV !== "production") {
+    middleware.push(createLogger());
 }
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancers(
-    applyMiddleware(...middleware)
-  ));
+const store = createStore(
+    persistedReducer,
+    composeEnhancers(applyMiddleware(...middleware))
+);
+
+let persistor = persistStore(store);
 
 store.dispatch(formsQuery().runMocked());
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>,
+    document.getElementById("root")
 );
