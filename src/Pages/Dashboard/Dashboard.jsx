53b93b3aa35340ef245befc41da54e547e8071e7
alexcharlton93 <alex-charlton@live.co.uk>
2021-04-20 19:33:26 +0100

    Added dashboard and global scss files

---
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Dashboard = () => {
+
+  return (
+    <div className="dashboard-wrapper">
+      <div className="dashboard-content">
+        <h1>Dashboard</h1>
+      </div>
+    </div>
+  );
+
+}
+
+export default Dashboard;
